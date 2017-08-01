893f7f52fb98b55d0d32c3942e8ffa1b655582d9
asmw <asmw@asmw.org>
2017-08-01 21:35:03 +0200

    Updates to qr code generation
    - Distinguish hotp/totp (although hotp is not yet supported)
    - Use the provided interfal/period
    - Allow some special characters in the filename
    - Split the label into issuer and label if the sequence " - " is found

--- generate_qr_codes.py
+++ generate_qr_codes.py
@@ -14,9 +14,19 @@ for line in fileinput.input(args):
 entries = json.loads(text)
 
 for entry in entries:
-    totp = pyotp.TOTP(entry['secret'])
-    url = totp.provisioning_uri(entry['label'])
-    img = qrcode.make(url)
-    safe_filename = "".join([c for c in entry['label'] if c.isalpha() or c.isdigit()]).strip() + ".png"
-    img.save(safe_filename)
-    print("Code saved as: %s" % safe_filename)
+    url = None
+    issuer = None
+    label = entry['label']
+    if " - " in label:
+        issuer, label = label.split(" - ", 1)
+    if entry['type'] == 'TOTP':
+        totp = pyotp.TOTP(entry['secret'], interval=entry['period'])
+        url = totp.provisioning_uri(label, issuer_name = issuer)
+    elif entry['type'] == 'HOTP':
+        totp = pyotp.TOTP(entry['secret'])
+        url = totp.provisioning_uri(label, issuer_name = issuer)
+    if url:
+        img = qrcode.make(url)
+        safe_filename = "".join([c for c in label if c.isalpha() or c.isdigit() or c in "@_-"]).strip() + ".png"
+        img.save(safe_filename)
+        print("Code saved as: %s" % safe_filename)
