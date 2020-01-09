536c643cc8e90c865ae1bd9ebb9910287acfd95b
asmw <asmw@asmw.org>
2020-01-09 22:22:33 +0100

    Add test shell script

---
+++ testdata/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+echo 123456 | python ../andotp_decrypt.py accounts_new_123456.json.aes
+echo 123456 | python ../generate_code.py accounts_new_123456.json.aes test
+echo 123456 | python ../generate_qr_codes.py accounts_new_123456.json.aes
+
+echo 123456 | python ../andotp_decrypt.py -o accounts_old_123456.json.aes
+echo 123456 | python ../generate_code.py -o accounts_old_123456.json.aes test
+echo 123456 | python ../generate_qr_codes.py -o accounts_old_123456.json.aes
